372445e0a01a05d6bb0a1e25c5cb688968664a2d
feedblackg44 <pilmyh@gmail.com>
2020-11-24 18:27:04 +0200

    lab5 waits for final test

--- Lab5/Lab5/main.cpp
+++ Lab5/Lab5/main.cpp
@@ -56,7 +56,7 @@ int main()
             }
 
             string divider;
-            if ((amount % 10 <= 4 || amount % 10 >= 2) && (amount % 100 < 12 || amount % 100 > 14))
+            if (amount % 10 <= 4 && amount % 10 >= 2 && (amount % 100 < 12 || amount % 100 > 14))
                 divider = " делителя";
             else if (amount % 10 == 1 && amount % 100 != 11)
                 divider = " делитель";

7a6b8f6d0e30faaaa268ecfe4490eb168115d33f
feedblackg44 <pilmyh@gmail.com>
2020-11-24 10:13:55 +0200

    Lab5 files created

---
+++ Lab5/Lab5/functions.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+#include <limits>
+#include <locale.h>
+#include <iomanip>
+#include "feed.h"
+#include "functions.h"
+
+using namespace std;
+
+// Functions here
---
+++ Lab5/Lab5/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+#include <limits>
+#include <locale.h>
+#include <iomanip>
+#include "feed.h"
+#include "functions.h"
+
+using namespace std;
+
+int main()
+{
+	SetDefaults();
+	
+	
+
+	SystemPause();
+	return 0;
+}

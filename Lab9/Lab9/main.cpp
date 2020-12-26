34732205bf994b0c4c044b259da29a0ad88ff3bd
feedblackg44 <pilmyh@gmail.com>
2020-12-26 15:33:58 +0200

    Lab9 report change

--- Lab9/Lab9/main.cpp
+++ Lab9/Lab9/main.cpp
@@ -10,11 +10,11 @@ int main()
     SetDefaults();
     LabHeader(9);
     cout << "Программа убирает из заданной строки слова на чётных местах\nСреди оставшихся находит и выводит самое длинное\n" << endl;
-
-    string  strIn,                      // Строка для хранения ввода пользователя
-            strMax,                     // Самое длинное слово
-            strOut;                     // Строка для вывода
     
+    string strIn,                      // Строка для хранения ввода пользователя
+           strMax,                     // Самое длинное слово
+           strOut;                     // Строка для вывода
+
     GetInputString(&strIn);
 
     SplitStrings(strIn, &strOut, &strMax);
